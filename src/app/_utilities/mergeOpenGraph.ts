a6ac4643b0319a83f88560dad07e467696f5325a
therealchrisrock <therealchrisrock@proton.me>
2024-11-05 11:15:09 -0500

    updated workflow file + spacing for form block

--- src/app/_utilities/mergeOpenGraph.ts
+++ src/app/_utilities/mergeOpenGraph.ts
@@ -2,12 +2,12 @@ import type { Metadata } from 'next'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  siteName: 'Payload E-Commerce Template',
-  title: 'Payload E-Commerce Template',
-  description: 'An open-source e-commerce store built with Payload and Next.js.',
+  siteName: 'Casa Bambu',
+  title: 'Casa Bambu',
+  description: 'Experience the ultimate vacation at Casa Bambu in West Bay, Roatan, Honduras. Our luxurious rental properties offer stunning views, modern amenities, and easy access to pristine beaches. Book your stay today and enjoy a tropical paradise like no other.',
   images: [
     {
-      url: 'https://payloadcms.com/images/og-image.jpg',
+      url: 'https://res.cloudinary.com/deep2qpb8/image/upload/f_auto,q_auto/v1/media/llcl08mz6d9mymlxe5ct',
     },
   ],
 }
